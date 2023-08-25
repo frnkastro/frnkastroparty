7c98f8d5f310772da4d264713e1a176dbc4d2221
frnkastro <frnkastroparty@gmail.com>
2023-08-25 14:59:12 -0600

    adjust particles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
 import profilepic from './profilepic.jpg'
-import Particles from 'react-particles';
-import tsParticleConfig from './tsParticleConfig';
 
 const App = () => {
 
   return (
     <main>
-      <Particles id="tsparticles" options={tsParticleConfig} />
-      <img src={profilepic} className="profile-pic" alt="frnk astro in space"/>
+      <a href='https://frnkastro.bandcamp.com'>
+        <img src={profilepic} className="profile-pic" alt="frnk astro in space"/>
+      </a>
       <div className="firefly"></div>
       <div className="firefly"></div>
       <div className="firefly"></div>
