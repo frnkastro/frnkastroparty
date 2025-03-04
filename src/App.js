4bf8c3f7f006bcdb4594fd4f985e1bd7d01b69e0
frnkastro <frnkastroparty@gmail.com>
2025-03-04 14:08:24 -0700

    modify seo elements

--- src/App.js
+++ src/App.js
@@ -7,15 +7,13 @@ const App = () => {
         <h1 className="band-name">frnk astro</h1>
         <p className="album-announcement">Full-length album coming summer 2025</p>
         <div className="button-container">
-          <a href="https://frnkastro.bandcamp.com" target="_blank" className="link-btn">Bandcamp</a>
-          <a href="https://soundcloud.com/frnkastro" target="_blank" className="link-btn">SoundCloud</a>
+          <a href="https://frnkastro.bandcamp.com" target="_blank" className="link-btn" aria-label="Listen to frnk astro on Bandcamp">Bandcamp</a>
+          <a href="https://soundcloud.com/frnkastro" target="_blank" className="link-btn" aria-label="Listen to frnk astro on SoundCloud">SoundCloud</a>
           {/* <a href="https://open.spotify.com/artist/YOUR-ARTIST-ID" target="_blank" className="link-btn">Spotify</a> */}
-          <a href="https://instagram.com/frnkastro" target="_blank" className="link-btn">Instagram</a>
-          <a href="https://facebook.com/frnkastro" target="_blank" className="link-btn">Facebook</a>
+          <a href="https://instagram.com/frnkastro" target="_blank" className="link-btn" aria-label="Follow frnk astro on Instagram">Instagram</a>
+          <a href="https://facebook.com/frnkastro" target="_blank" className="link-btn" aria-label="Follow frnk astro on Facebook">Facebook</a>
         </div>
       </div>
     </main>
   );
 }
-
-export default App;
