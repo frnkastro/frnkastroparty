14d5e2bf99baa6b1e33d9b547c39885b8a293828
frnkastro <frnkastroparty@gmail.com>
2025-12-01 09:25:11 -0700

    update app

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ const App = () => {
           <a href="https://frnkastro.bandcamp.com" target="_blank" className="link-btn" aria-label="Listen to frnk astro on Bandcamp">Bandcamp</a>
           <a href="https://soundcloud.com/frnkastro" target="_blank" className="link-btn" aria-label="Listen to frnk astro on SoundCloud">SoundCloud</a>
           {/* <a href="https://open.spotify.com/artist/YOUR-ARTIST-ID" target="_blank" className="link-btn">Spotify</a> */}
-          <a href="https://instagram.com/frnkastro" target="_blank" className="link-btn" aria-label="Follow frnk astro on Instagram">Instagram</a>
-          <a href="https://facebook.com/frnkastro" target="_blank" className="link-btn" aria-label="Follow frnk astro on Facebook">Facebook</a>
+          {/* <a href="https://instagram.com/frnkastro" target="_blank" className="link-btn" aria-label="Follow frnk astro on Instagram">Instagram</a> */}
+          {/* <a href="https://facebook.com/frnkastro" target="_blank" className="link-btn" aria-label="Follow frnk astro on Facebook">Facebook</a> */}
         </div>
       </div>
     </main>
